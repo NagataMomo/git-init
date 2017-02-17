638c208e82a2aa287ec97e2521c49d83cb16ff7e
MomoNagata <momo.113sk@gmail.com>
2017-02-17 13:14:22 +0900

    add

--- Babule.py
+++ Babule.py
@@ -1,10 +1,17 @@
 a=[9,6,7,1,2]
 
-for i in range(4):
-   if  a[ i ] > a[i + 1]:
-       t=a[i]
-       a[i]=a[i+1]
-       a[i+1]=t
 
+
+#for j  in range(len(a)):
+
+i=0
+while True:
+
+        if    (a[ i ] < a[i + 1])& (i>=4 ):
+        break
+    t=a[i]
+    a[i]=a[i+1]
+    a[i+1]=t
+    i+=1
 
 print(a)
